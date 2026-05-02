088839c287708a87008f1c154579b63e13bf5bce
McbeEringi <git@mcbeeringi.dev>
2026-05-02 21:03:53 +0900

    -

--- rosenzu/index.mjs
+++ rosenzu/index.mjs
@@ -1,12 +1,9 @@
-#!/bin/env node
-// #!/bin/bun --intall=always
+#!/bin/bun --intall=always
 import{parse}from'toml';
 import{rosenzu}from'./rosenzu.mjs';
-import{readFile}from'node:fs/promises'
 
 console.log(
 	rosenzu(parse(
-		await readFile('data.toml','utf8')
-		// await Bun.file('data.txt').text()
+		await Bun.file('data.toml').text()
 	))
 )
